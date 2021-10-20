ba5111bd11e104d21628574fab624690bd7d80ef
siroz1907 <sabrisezer@gmail.com>
2021-10-20 17:23:40 +0300

    firebase info hidden

--- src/options/firebase.js
+++ src/options/firebase.js
@@ -6,13 +6,13 @@ import { initializeApp } from 'firebase/app';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCQswwSY8XZrKN6zq4hGLgzi-QRJxWoExY",
-    authDomain: "kinetic-road-247823.firebaseapp.com",
-    databaseURL: "https://kinetic-road-247823.firebaseio.com",
-    projectId: "kinetic-road-247823",
-    storageBucket: "kinetic-road-247823.appspot.com",
-    messagingSenderId: "931981278408",
-    appId: "1:931981278408:web:21d83f0f42788e86d0b9fd"
+    apiKey: "your-api-key",
+    authDomain: "yours",
+    databaseURL: "yours",
+    projectId: "your project id",
+    storageBucket: "your",
+    messagingSenderId: "your",
+    appId: "your-appid"
   };
 
   
